4827f6e2fd27702d665c27568ce2826ecc6b7642
nsmn38 <niklas.simonis@mni.thm.de>
2014-01-21 15:32:03 +0100

    codestyle

--- src/main/webapp/app/view/speaker/form/GridSquareQuestion.js
+++ src/main/webapp/app/view/speaker/form/GridSquareQuestion.js
@@ -1,62 +1,43 @@
 Ext.define('ARSnova.view.speaker.form.GridSquareQuestion', {
  extend: 'Ext.Container',
-
-
-
  constructor: function() {
   this.callParent(arguments);
 
-
-
   var gsCanvas = Ext.create('Ext.form.FieldSet', {
    html: "<div align='center'><canvas width='800' height='600' id='gsCanvas'></canvas></div>",
-   items: [
-{
+   items: [{
 	xtype: 'textfield',
 	placeHolder: 'Url eingeben!',
 	name: 'url',
 	id: 'imgurl',
-
-
    },
    {
 	 xtype : 'slider',
 	 renderTo: 'custom-slider',
      value: 3,
      increment: 1,
-     minValue: 1,
-     maxValue: 6,
+     minValue: 2,
+     maxValue: 5,
      id: 'slider',
      listeners : {
          change: function(slider, thumb, newVal, oldVal){
         	 planquadrat.raster.columns =Ext.getCmp('slider').getValue() ;
         	 planquadrat.raster.rows = Ext.getCmp('slider').getValue();
         	 planquadrat.init();
-         }}
+         }
+    }
    },
-
    {
-	   xtype : 'button',
-       itemId : 'upload',
-       text : 'Upload Picture',
-       handler : function(){
-
+	 xtype : 'button',
+     itemId : 'upload',
+     text : 'Upload Picture',
+     handler : function(){
     	  planquadrat.raster.columns =Ext.getCmp('slider').getValue() ;
       	  planquadrat.raster.rows = Ext.getCmp('slider').getValue();
     	  planquadrat.picture.loadPicture(Ext.getCmp('imgurl').getValue());
     	  planquadrat.init();
-    }}
-
-   ],
-
-
-
-  })
+    }
+  }],
+ });
  this.add([gsCanvas]);
-  //this.added( console.log('addedr listener'));
- //setTimeout(function (){planquadrat.init();}, 10000);
-
-
- //planquadrat.init();
- }}
-);
+}});
